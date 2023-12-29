3bc291579500bc39a5e48f7b5f5ed119034b3e68
JoaoCosme <cosmejoaopedro@protonmail.com>
2023-12-29 13:41:58 -0300

    ihc: add nav

---
+++ ihc_segunda_costura/lib/minha_loja.dart
@@ -0,0 +1,233 @@
+import 'package:flutter/material.dart';
+
+class MinhaLoja extends StatefulWidget {
+  const MinhaLoja({super.key});
+
+  @override
+  _MinhaLojaState createState() => _MinhaLojaState();
+}
+
+class _MinhaLojaState extends State<MinhaLoja> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          width: 360,
+          height: 800,
+          clipBehavior: Clip.antiAlias,
+          decoration: ShapeDecoration(
+            color: Colors.white,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+          child: Stack(
+            children: [
+              Positioned(
+                left: 15,
+                top: 26,
+                child: Container(
+                  width: 24,
+                  height: 24,
+                  clipBehavior: Clip.antiAlias,
+                  decoration: const BoxDecoration(),
+                  child: const Stack(children: []),
+                ),
+              ),
+              Positioned(
+                left: 15,
+                top: 81,
+                child: Container(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Minha Loja',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 24,
+                          fontFamily: 'Open Sans',
+                          fontWeight: FontWeight.w700,
+                          height: 0,
+                        ),
+                      ),
+                      const SizedBox(height: 23),
+                      Opacity(
+                        opacity: 0.40,
+                        child: Container(
+                          width: 330,
+                          padding: const EdgeInsets.only(
+                            top: 20,
+                            left: 12,
+                            right: 10,
+                            bottom: 20,
+                          ),
+                          decoration: ShapeDecoration(
+                            color: const Color(0xFFB618EE),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Container(
+                                child: const Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      'Adicionar Drop',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 16,
+                                        fontFamily: 'Open Sans',
+                                        fontWeight: FontWeight.w300,
+                                        height: 0,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 23),
+                      Opacity(
+                        opacity: 0.40,
+                        child: Container(
+                          width: 330,
+                          padding: const EdgeInsets.only(
+                            top: 20,
+                            left: 12,
+                            right: 10,
+                            bottom: 20,
+                          ),
+                          decoration: ShapeDecoration(
+                            color: const Color(0xFFB618EE),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          child: const Row(
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Row(
+                                mainAxisSize: MainAxisSize.min,
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    'Minhas Peças',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 16,
+                                      fontFamily: 'Open Sans',
+                                      fontWeight: FontWeight.w300,
+                                      height: 0,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 23),
+                      Opacity(
+                        opacity: 0.40,
+                        child: Container(
+                          width: 330,
+                          padding: const EdgeInsets.only(
+                            top: 20,
+                            left: 12,
+                            right: 10,
+                            bottom: 20,
+                          ),
+                          decoration: ShapeDecoration(
+                            color: const Color(0xFFB618EE),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          child: const Row(
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Row(
+                                mainAxisSize: MainAxisSize.min,
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    'Minhas Vendas',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 16,
+                                      fontFamily: 'Open Sans',
+                                      fontWeight: FontWeight.w300,
+                                      height: 0,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 286,
+                top: 25,
+                child: Container(
+                  width: 50,
+                  height: 50,
+                  decoration: const ShapeDecoration(
+                    color: Color(0xFFE2A3F8),
+                    shape: OvalBorder(),
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 299,
+                top: 38,
+                child: Container(
+                  width: 24,
+                  height: 24,
+                  padding: const EdgeInsets.symmetric(horizontal: 4),
+                  clipBehavior: Clip.antiAlias,
+                  decoration: const BoxDecoration(),
+                  child: const Row(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        width: 16,
+                        height: 24,
+                        child: Stack(children: []),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
