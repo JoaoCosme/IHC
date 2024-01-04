a453cc53ac1fd27771a86f1512cec6d218ae8ccf
JoaoCosme <cosmejoaopedro@protonmail.com>
2024-01-04 12:22:55 -0300

    ihc: add close button

--- ihc_segunda_costura/lib/admin/peca_editavel.dart
+++ ihc_segunda_costura/lib/admin/peca_editavel.dart
@@ -1,97 +1,115 @@
 import 'package:flutter/material.dart';
 
 class PecaEditavel extends StatelessWidget {
+  final VoidCallback? onDelete;
+
+  const PecaEditavel({Key? key, this.onDelete}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Material(
-        child: Column(
-      children: [
-        Container(
-          height: 10,
-          decoration: const BoxDecoration(color: Colors.white),
-        ),
-        Container(
-          width: 335,
-          height: 125,
-          decoration: ShapeDecoration(
-            color: const Color(0xC4E2A3F8),
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(0)),
+      child: Column(
+        children: [
+          Container(
+            height: 10,
+            decoration: const BoxDecoration(color: Colors.white),
           ),
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
+          Stack(
             children: [
               Container(
-                width: 71,
-                height: 95,
-                decoration: const ShapeDecoration(
-                  image: DecorationImage(
-                    image: NetworkImage(
-                        "https://imgs.search.brave.com/1WJ4SrN0fF6zWGPTa-9EOnOBtqZlRi4N_d1jjxvVFNw/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvNjQx/MDYyNTM4L3Bob3Rv/L2xlYXRoZXItamFj/a2V0LmpwZz9zPTYx/Mng2MTImdz0wJms9/MjAmYz14Vi1XMUw4/Snp6WURRWWJ5SXdw/WVN3b2Z2N2w3cUVY/UXduejJuYWdCem1z/PQ"),
-                    fit: BoxFit.fill,
-                  ),
+                width: 335,
+                height: 125,
+                decoration: ShapeDecoration(
+                  color: const Color(0xC4E2A3F8),
                   shape: RoundedRectangleBorder(
-                    side: BorderSide(
-                      width: 1,
-                      strokeAlign: BorderSide.strokeAlignOutside,
-                      color: Colors.white,
-                    ),
+                    borderRadius: BorderRadius.circular(0),
                   ),
                 ),
-              ),
-              const SizedBox(width: 21),
-              const SizedBox(
-                width: 200,
-                height: 70,
                 child: Row(
                   mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Nome da Peça #1',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 16,
-                            fontFamily: 'Open Sans',
-                            fontWeight: FontWeight.w700,
-                            height: 0,
+                    Container(
+                      width: 71,
+                      height: 95,
+                      decoration: const ShapeDecoration(
+                        image: DecorationImage(
+                          image: NetworkImage(
+                              "https://imgs.search.brave.com/1WJ4SrN0fF6zWGPTa-9EOnOBtqZlRi4N_d1jjxvVFNw/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvNjQx/MDYyNTM4L3Bob3Rv/L2xlYXRoZXItamFj/a2V0LmpwZz9zPTYx/Mng2MTImdz0wJms9/MjAmYz14Vi1XMUw4/Snp6WURRWWJ5SXdw/WVN3b2Z2N2w3cUVY/UXduejJuYWdCem1z/PQ"),
+                          fit: BoxFit.fill,
+                        ),
+                        shape: RoundedRectangleBorder(
+                          side: BorderSide(
+                            width: 1,
+                            strokeAlign: BorderSide.strokeAlignOutside,
+                            color: Colors.white,
                           ),
                         ),
-                        Text(
-                          'Descrição da Peça',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 12,
-                            fontFamily: 'Open Sans',
-                            fontWeight: FontWeight.w300,
-                            height: 0,
-                          ),
-                        ),
-                        Text(
-                          'R\$ 50,00',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 12,
-                            fontFamily: 'Open Sans',
-                            fontWeight: FontWeight.w300,
-                            height: 0,
-                          ),
-                        ),
-                      ],
-                    )
+                      ),
+                    ),
+                    const SizedBox(width: 21),
+                    const SizedBox(
+                      width: 200,
+                      height: 70,
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Nome da Peça #1',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 16,
+                                  fontFamily: 'Open Sans',
+                                  fontWeight: FontWeight.w700,
+                                  height: 0,
+                                ),
+                              ),
+                              Text(
+                                'Descrição da Peça',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 12,
+                                  fontFamily: 'Open Sans',
+                                  fontWeight: FontWeight.w300,
+                                  height: 0,
+                                ),
+                              ),
+                              Text(
+                                'R\$ 50,00',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 12,
+                                  fontFamily: 'Open Sans',
+                                  fontWeight: FontWeight.w300,
+                                  height: 0,
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
                   ],
+                ),
+              ),
+              Positioned(
+                top: 5,
+                right: 5,
+                child: IconButton(
+                  icon: Icon(Icons.close),
+                  onPressed: onDelete,
                 ),
               ),
             ],
           ),
-        ),
-      ],
-    ));
+        ],
+      ),
+    );
   }
 }
